c83801f2a0e81e63c949f69c8ac92e6aac8b945b
hillarymuir <hillarymuir@pm.me>
2026-01-20 12:44:58 -0500

    test: write tests for markdown_to_html_node

--- src/test_functions.py
+++ src/test_functions.py
@@ -360,3 +360,89 @@ This is the same paragraph on a new line
             blocktype,
             BlockType.PARAGRAPH
         )
+
+    # test block to HTML
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    )
+        
+    def test_unordered_list(self):
+        md = """
+- This is **bolded** paragraph
+- This is another paragraph with _italic_ text and `code` here
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is <b>bolded</b> paragraph</li><li>This is another paragraph with <i>italic</i> text and <code>code</code> here</li></ul></div>",
+        )
+        
+    def test_ordered_list(self):
+        md = """
+1. This is **bolded** paragraph
+2. This is another paragraph with _italic_ text and `code` here
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>This is <b>bolded</b> paragraph</li><li>This is another paragraph with <i>italic</i> text and <code>code</code> here</li></ol></div>",
+        )
+
+    def test_h1_blockquote(self):
+        md = """
+# Here are some cool quotes:
+
+> This quote is 
+> so neat, right?
+
+> This one too, right?
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>Here are some cool quotes:</h1><blockquote>This quote is so neat, right?</blockquote><blockquote>This one too, right?</blockquote></div>",
+        )
+
+    def test_empty_string_to_html(self):
+        md = ""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div></div>",
+        )
