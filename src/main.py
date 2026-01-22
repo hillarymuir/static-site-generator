f5da170b7035cbcfa3ad1b82f52abeca19cdcac4
hillarymuir <hillarymuir@pm.me>
2026-01-22 10:38:47 -0500

    feat: copy static

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import textnode as tn
 
-def copy_contents_to_destination(source_dir, destination_dir):
+def copy_contents_to_destination(source_dir, destination_dir, parent):
     # ensure paths exist and convert to absolute paths if relative paths were input
     if not os.path.exists(source_dir):
         try:
@@ -16,21 +16,35 @@ def copy_contents_to_destination(source_dir, destination_dir):
             destination_dir = f"{os.path.abspath(destination_dir)}"
         except [ValueError]:
             print("Error: destination directory does not exist")
+    elif not os.path.exists(parent):
+        try:
+            destination_dir = f"{os.path.abspath(parent)}"
+        except [ValueError]:
+            print("Error: parent directory does not exist")
 
-    # delete contents of destination
-    shutil.rmtree(destination_dir)
-    os.mkdir("/home/hillary/static-site-generator/src/public")
 
-    # TODO copy all files, subdirectories, and nested files
+    # delete contents of destination if it is public so there is a fresh slate
+    if destination_dir == parent:
+        shutil.rmtree(parent)
+        os.mkdir(parent)
 
-    # TODO log path of each copied file for debugging
+    # copy all files, subdirectories, and nested files
+    for entry in os.listdir(source_dir):
+        entry_path = os.path.join(source_dir, entry)
+        if os.path.isfile(entry_path):
+            shutil.copy(entry_path, destination_dir)
+            log_path = os.path.abspath("copy_log.txt")
+            with open(log_path, "a") as f:
+                f.write(f"Copying {entry_path} to {destination_dir}\n")
+        else:
+            new_destination_path = os.path.join(destination_dir, entry)
+            os.mkdir(new_destination_path)
+            copy_contents_to_destination(entry_path, new_destination_path, parent)
 
 
 def main():
     print("Hello from static-site-generator!")
-
-    print("Testing emptying of public...")
-    copy_contents_to_destination("src/static", "src/public")
+    copy_contents_to_destination("static", "src/public", "src/public")
 
 
 if __name__ == "__main__":
